0d6c4c9ce55307a9aa2f8dbf69ef118963e81004
Lorenzo Young <lyoun306@mtroyal.ca>
2020-11-20 12:59:03 -0700

    Added Resources files and created PHP, Javascript and CSS files for the Assignment. Feel free to add or edit the files and filesnames as needed

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="COMP3512 Assignment02">
+    <meta name="author" content="">
+    <title>COMP 3512 Assign1</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/jscript.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
