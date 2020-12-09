dd470491c41c59dfd9b260c4ef747f075b2a821c
Ryan J. Wong <rwong210@mtroyal.ca>
2020-12-09 16:47:18 -0700

    implementing addToFavorites.php

---
+++ addToFavorites.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['favorites'])){
+        // does not exist, so initalize it
+        $_SESSION["favorites"] = [];
+    }
+    // retrieve any existing favorites
+    $fav = $_SESSION["favorites"];
+
+    // TODO ensure querystring exists
+    // now add passed painting ID to the array
+    $fav[] = $_GET['id'];
+
+    //resave modified array back to session state
+    $_SESSION["favorites"] = $fav;
+
+    //redirect back to page that requested this
+    header("Location:" . $_Server["HTTP_REFFERER"]);
+?>
