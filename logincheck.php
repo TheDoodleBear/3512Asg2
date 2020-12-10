188130f1c6661312c619042eeca845dd555b0e99
Ryan J. Wong <rwong210@mtroyal.ca>
2020-12-10 13:50:16 -0700

    added missing code to blank page

---
+++ logincheck.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'dbconfig.inc.php';
+require_once 'db-class.inc.php';
+$conn = DatabaseConn::establishConn(array(DBCONNSTRING, DBUSER, DBPASS));
+$dbConnect = new loginDB($conn);
+if (isset($_POST['submit'])) {
+    if (empty($_POST['uname']) || empty($_POST['psw'])) {
+        header("Location:../login.php?error=empty");
+        exit();
+    } else {
+        $userCred = $dbConnect->checkUser($_POST['uname']);
+        if (!empty($userCred)) {
+            if (password_verify($_POST['psw'], $userCred['Pass'])) {
+                session_start();
+                $_SESSION['CustID'] =  $userCred['CustomerID'];
+                header("Location:../index.php");
+            } else {
+                header("Location:../login.php?error=invalidpass");
+            }
+        }
+    }
+} else {
+    header("Location:../login.php?error=nowork");
+    exit();
+}
