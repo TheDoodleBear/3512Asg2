9e27f6bf3f2f2a923b23f2c1b92ee68dd3fb1981
Lorenzo Young <lyoun306@mtroyal.ca>
2020-12-10 18:19:18 -0700

    Completed the recommendation section for home page

--- includes/logincheck.php
+++ includes/logincheck.php
@@ -1,25 +1,31 @@
 <?php
 require_once 'dbconfig.inc.php';
 require_once 'db-class.inc.php';
-$conn = DatabaseConn::establishConn(array(DBCONNSTRING, DBUSER, DBPASS));
-$dbConnect = new loginDB($conn);
-if (isset($_POST['submit'])) {
-    if (empty($_POST['uname']) || empty($_POST['psw'])) {
-        header("Location:../login.php?error=empty");
-        exit();
-    } else {
-        $userCred = $dbConnect->checkUser($_POST['uname']);
-        if (!empty($userCred)) {
-            if (password_verify($_POST['psw'], $userCred['Pass'])) {
-                session_start();
-                $_SESSION['CustID'] =  $userCred['CustomerID'];
-                header("Location:../index.php");
-            } else {
-                header("Location:../login.php?error=invalidpass");
+try {
+    $conn = DatabaseConn::establishConn(array(DBCONNSTRING, DBUSER, DBPASS));
+    $dbConnect = new loginDB($conn);
+    if (isset($_POST['submit'])) {
+        if (empty($_POST['uname']) || empty($_POST['psw'])) {
+            header("Location:../login.php?error=empty");
+            exit();
+        } else {
+            $userCred = $dbConnect->checkUser($_POST['uname']);
+            if (!empty($userCred)) {
+                if (password_verify($_POST['psw'], $userCred['Pass'])) {
+                    session_start();
+                    $_SESSION['CustID'] =  $userCred['CustomerID'];
+                    header("Location:../index.php");
+                    exit();
+                } else {
+                    header("Location:../login.php?error=invalidpass");
+                    exit();
+                }
             }
         }
+    } else {
+        header("Location:../login.php?error=nowork");
+        exit();
     }
-} else {
-    header("Location:../login.php?error=nowork");
-    exit();
+} catch (Exception $e) {
+    die($e->getMessage());
 }
--- single-painting.php
+++ single-painting.php
@@ -17,7 +17,6 @@ try {
 
 
 // if the the customer is logged in and the painting is not already a favorite display the add to favorites button
-// if (isset($_SESSION["CustID"])) {
 if (isset($_SESSION['favorites'])) {
     if (isFavorite($painting)) {
         // sets the default visibility to hidden fo
@@ -25,32 +24,48 @@ if (isset($_SESSION['favorites'])) {
     } else {
         $visibility = "visible";
     }
-}else {
+} else {
     $visibility = "visible";
 }
-// }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="COMP3512 Assignment02">
-    <meta name="author" content="">
-    <title>COMP 3512 Assign1</title>
-    <!-- check ref path for styling page if there is an error in your instance -->
-    <link rel="stylesheet" type="text/css" href="css/sng-paint-css.php" media="screen" />
-    <!-- <style>
-        <?php #include "./css/sng-paint-css.css" 
-        ?>
-    </style> -->
+    <meta name="author" content="Lorenzo Young">
+    <title>COMP 3512 Assign2</title>
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.php" media="screen"/> -->
+    <link rel="stylesheet" type="text/css" href="css/app.css" />
+    <link rel="stylesheet" type="text/css" href="css/sng-paint-css.css" />
     <script src="js/sng-paint.js"></script>
 </head>
 
 <body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">Assignment2</label>
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="galleries.php">Galleries</a></li>
+            <li><a href="browse-paintings.php">Browse/Search Paintings</a></li>
+            <li><a href="favorites.php">Favorites</a></li>
+            <?php
+            if (isset($_SESSION['CustID'])) {
+                echo "<li><a href='logout.php'>LogOut</a></li>";
+            } else {
+                echo "<li><a href='login.php'>Login</a></li>";
+            }
+            ?>
+        </ul>
+    </nav>
     <main id="singlePaintPage">
         <div class="headr">
             <h1>Painting Details</h1>
