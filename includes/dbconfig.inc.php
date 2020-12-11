4498a490b99d9165b2c5569efe79cff5854c3fed
Lorenzo Young <lyoun306@mtroyal.ca>
2020-12-10 18:37:05 -0700

    edited the db-config to reflect online hosting

--- includes/dbconfig.inc.php
+++ includes/dbconfig.inc.php
@@ -4,12 +4,16 @@
 // define('DBUSER', getenv('MYSQL_USER') );
 // define('DBPASS', getenv('MYSQL_PASSWORD') );
 
-define('DBHOST', 'localhost');
-define('DBNAME', 'art');
-define('DBUSER', 'root');
-define('DBPASS', '');
+//define('DBHOST', 'localhost');
+//define('DBNAME', 'art');
+//define('DBUSER', 'root');
+//define('DBPASS', '');
+//define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+
+define('DBHOST', 'us-cdbr-east-02.cleardb.com');
+define('DBNAME', 'Comp3512A2Dec-7');
+define('DBUSER', 'bde88340f4f08c');
+define('DBPASS', 'b3c67720');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
-
-
-
