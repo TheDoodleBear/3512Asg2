8dfb841b2aef2c2fb39e36298e8170d00708e77d
Ryan J. Wong <rwong210@mtroyal.ca>
2020-12-10 18:23:20 -0700

    changed DBNAME to -7 from 7

--- includes/dbconfig.inc.php
+++ includes/dbconfig.inc.php
@@ -12,7 +12,7 @@
 
 
 define('DBHOST', 'us-cdbr-east-02.cleardb.com');
-define('DBNAME', 'Comp3512A2Dec7');
+define('DBNAME', 'Comp3512A2Dec-7');
 define('DBUSER', 'bde88340f4f08c');
 define('DBPASS', 'b3c67720');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
